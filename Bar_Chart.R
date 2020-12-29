524b727c53111c5b8fa85c4eca833ce082f27590
jamisonbrogdon <jamisonbrogdon@gmail.com>
2020-12-29 16:41:28 -0700

    added barchart file

---
+++ Bar_Chart.R
@@ -0,0 +1,33 @@
+B <- c(22, 27, 26, 24, 23)
+
+barplot(B)
+# barchart with added parameters
+barplot(B,
+        main = "Company B Stock Prices",
+        xlab = "Week End 9/26",
+        ylab = "Price",
+        ylim = c(0,30),
+        names.arg = c('Mon', 'Tue', 'Wed', 'Thu', 'Fri'),
+        col = colors()[12],
+        horiz = FALSE)
+
+# Create data
+set.seed(112)
+Z = matrix(
+  c(15, 13, 18, 55, 60, 35, 35, 38, 41),
+  nrow = 3,
+  ncol = 3,
+  byrow = TRUE)
+dimnames(Z) = list(
+  c('Sell', 'Hold', "Buy"),
+  c('A', 'B', "C")
+)
+
+# Get the stacked barplot
+barplot (Z)
+barplot(Z,
+        col = colors() [c(35, 77, 89)],
+        border = "white",
+        space = 0.04,
+        font.axis = 2,
+        xlab = "group")
